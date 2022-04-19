db9627b6b5869a5566b7a68bb07e2eb1309aed5a
Daniel Rosas <knd0326@gmail.com>
2022-04-18 19:21:50 -0500

    2 step: main structure, input and show screen

--- 2_hangman/2_game_hangman.py
+++ 2_hangman/2_game_hangman.py
@@ -1,5 +1,5 @@
 import random 
-
+import os,sys
 def select():
     a=random.randint(0,214)
     with open("./2_hangman/word_hangman.txt","r",encoding="utf-8") as f:
@@ -8,5 +8,27 @@ def select():
         word.pop()
         del words     
     return word    
-    
-print(select())
+
+lives=8
+word=select()
+print(word)
+space_word= []
+for i in word: space_word.append("_")
+while space_word != word:
+    b=len(space_word)
+    if lives>0:
+        for i in range (0,b): print(space_word[i],end=" ")
+        letter= input("\n \nwrite a letter[a-z]: ")
+        comprobation= letter in word
+        if comprobation:
+            for i in range(0,b):
+                if letter==word[i]:space_word[i]=letter
+        else:
+            lives-=1
+    os.system("cls")
+    if space_word == word:
+        for i in range (0,b): print(space_word[i],end=" ")
+        print("\n \n ¡CONGRATULATIONS!")
+    if lives==0:
+        print("¡you lose the game!")
+        break
