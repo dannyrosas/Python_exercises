d318cc2b698ff7c39840d6ad81f89955a0dd5310
Daniel Rosas <knd0326@gmail.com>
2022-04-18 20:53:37 -0500

    3 step: exceptions and bugs

--- 2_hangman/2_game_hangman.py
+++ 2_hangman/2_game_hangman.py
@@ -1,5 +1,9 @@
+from pickle import FALSE
 import random 
 import os,sys
+
+alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
 def select():
     a=random.randint(0,214)
     with open("./2_hangman/word_hangman.txt","r",encoding="utf-8") as f:
@@ -14,17 +18,32 @@ word=select()
 print(word)
 space_word= []
 for i in word: space_word.append("_")
+b=len(space_word)
 while space_word != word:
-    b=len(space_word)
-    if lives>0:
-        for i in range (0,b): print(space_word[i],end=" ")
-        letter= input("\n \nwrite a letter[a-z]: ")
+    if lives>0:    
+        control=1
+        while control==1:
+            for i in range (0,b): print(space_word[i],end=" ")
+            try:
+                print(f"\n \n \nyou have {lives} lives")
+                letter= input("write a letter[a-z]: ")
+                letter=letter.lower()
+                pools= letter in alphabet                
+                if pools:
+                    control=2
+                else:
+                    raise ValueError
+            except ValueError:
+                os.system("cls")
+                print("you only can input a caracter bweteen in [a-z]")
+
         comprobation= letter in word
         if comprobation:
             for i in range(0,b):
                 if letter==word[i]:space_word[i]=letter
         else:
             lives-=1
+
     os.system("cls")
     if space_word == word:
         for i in range (0,b): print(space_word[i],end=" ")
